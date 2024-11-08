e4fa3990813364f0a0b5ca166913fb3044c45dea
peeweek <4037271+peeweek@users.noreply.github.com>
2024-11-08 10:55:27 +0100

    Removed unused using

--- Unity/net.peeweek.deploy-runner/Editor/DeployRunner.cs
+++ Unity/net.peeweek.deploy-runner/Editor/DeployRunner.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.NetworkInformation;
 using System.Linq;
-using static PlasticGui.LaunchDiffParameters;
 using System.Text.RegularExpressions;
 
 public class DeployRunner
